2b4e36a53fc7e21a17014c3fa0cc67f952fa2da1
John Ruffer <jqruffer@vt.edu>
2012-08-24 11:28:53 -0400

    first commit

---
+++ PyDateTime.py
@@ -0,0 +1,23 @@
+
+class DateTime:
+	def __init__(self):
+		self.month = 1
+		self.day = 1
+		self.year = 2012
+		self.hour = 0
+		self.minute = 0
+		self.second = 0
+
+	def getDate(self):
+		return [self.month, self.day, self.year]
+
+	def getTime(self):
+		return [self.hour, self.minute, self.second]
+
+	def getCsv(self):
+		return "%s,%s,%s,%s,%s,%s" % (self.month, self.day, self.year, \
+			self.hour, self.minute, self.second)
+
+	def toString(self):
+		return "%02d/%02d/%02d %02d:%02d:%02d" % (int(self.month),int(self.day), int(self.year), \
+			int(self.hour), int(self.minute), int(self.second))
