5f3989338a1c76ae77d49c1dd9b0750c8a22d460
John Ruffer <jqruffer@vt.edu>
2012-09-09 12:18:16 -0400

    wiped out most of dead code

--- arduino/SunLamp/SunLamp.ino
+++ arduino/SunLamp/SunLamp.ino
@@ -1,12 +1,16 @@
 #include <Time.h>
 
-unsigned greenChange[72] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
-                             1, 0, 2, 0, 2, 0, 2, 0, 3, 0, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4,
-                             4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 7, 7, 8, 8, 8, 8, 8, 8, 10, 10,
-                             10, 11, 11, 11, 11, 11
-                           };
+int NUMBER_OF_INTERVALS = 72;
 
-unsigned blueChange[72] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+int greenChange[NUMBER_OF_INTERVALS] = 
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+                            1, 0, 2, 0, 2, 0, 2, 0, 3, 0, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4,
+                            4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 7, 7, 8, 8, 8, 8, 8, 8, 10, 10,
+                            10, 11, 11, 11, 11, 11
+                          };
+
+int blueChange[NUMBER_OF_INTERVALS] =
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
                             0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 2, 1, 1, 2, 1, 1,
                             2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2,
                             2, 2, 2, 3, 3, 4
@@ -19,30 +23,15 @@ int bluePin = 7;
 int red;
 int green;
 int blue;
-int transitionTime;
 
-int getIncrementor( int oldValue, int newValue );
-
-void setup()
-{
+void setup() {
   pinMode( redPin, OUTPUT );
   pinMode( greenPin, OUTPUT );
   pinMode( bluePin, OUTPUT );
   
-  randomSeed( analogRead(0) );
   Serial.begin( 9600 );
-  red = 100;
-  green = 100;
-  blue = 100;
-  transitionTime = 0;
 }
 
-void loop()
-{
+void loop() {
   
-}
-
-int getIncrementor( int oldValue, int newValue )
-{
-  return ( oldValue - newValue ) < 0 ? 1 : -1;
 }
